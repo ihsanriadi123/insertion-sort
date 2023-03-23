acddf05fbd1f1b5c21b7974890df25344cf7de01
ihsanriadi123 <iputrariadi@gmail.com>
2023-03-23 12:02:35 +0700

    memasukan data nilai n kedalam array

--- belajar insertion sort/belajar insertion sort/belajar insertion sort.cpp
+++ belajar insertion sort/belajar insertion sort/belajar insertion sort.cpp
@@ -18,3 +18,14 @@ void input() {					// Procedure Input
 			cout << "\nArray yang anda masukkan maksimal 20 elemen,\n";	// Menampilkan Pesan jika data lebih dari 20
 		}
 	}
+	cout << endl;									// Membuat jarak per baris program
+	cout << "======================" << endl;		// Membuat tampilan susunan data element array
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++)						// Membuat perulangan for untuk menyimpan data pada array
+	{
+		cout << "Data ke-" << (i + 1) << ":";		// Memasukkan atau menginputkan nilai data n 
+		cin >> arr[i];								// Menyimpan nilai data n kedalam array arr
+	}
+}
